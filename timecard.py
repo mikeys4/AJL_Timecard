50160281316c0be2a209b8f10a8c54f8464e784b
Michael Sutherland <cat.sake@gmail.com>
2015-06-29 22:38:21 -0700

    AJL Timecard 0.1.2
    Timecard now writes to DB.
    
    Writes ALL DATA.

--- timecard.py
+++ timecard.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.before_request
 def before_request():
-    g.db = sqlite3.connect("users.db")
+    g.db = sqlite3.connect("timecards.db")
 
 @app.teardown_request
 def teardown_request(exception):
@@ -23,6 +23,29 @@ def hello_world():
 def signup():
 	username = request.form['username']
 	g.db.execute("INSERT INTO user_names VALUES (?)",[username])
+	g.db.commit()
+	return redirect('/')
+
+@app.route('/submit_paycheck', methods = ['POST'])
+def submit_paycheck():
+	Employee = request.form.get('Employee')
+	EmployeeID = 1
+	if Employee == 'Rob':
+		EmployeeID = 1
+	elif Employee == 'Rick':
+		EmployeeID = 2
+	elif Employee == 'Mike':
+		EmployeeID = 3
+	Date = request.form.get('Date')
+	TimeIn = request.form.get('TimeIn')
+	TimeOut = request.form.get('TimeOut')
+	RegHours = request.form.get('RegHours')
+	PtFiveTime = request.form.get('PtFiveTime')
+	TwoXTime = request.form.get('TwoXTime')
+	Sick = request.form.get('Sick')
+	Vac = request.form.get('Vac')
+	Holiday = request.form.get('Holiday')
+	g.db.execute("INSERT INTO timecards (EmployeeID, WorkDay, TimeIn, TimeOut, RegHours, PtFiveTime, TwoXTime, Sick, Vac, Holiday) VALUES (?,?,?,?,?,?,?,?,?,?);", (EmployeeID, Date, TimeIn, TimeOut, RegHours, PtFiveTime, TwoXTime, Sick, Vac, Holiday))
 	g.db.commit()
 	return redirect('/')
 	
